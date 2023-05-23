20cc5501093d50559c1c909cce5e5200102ed163
Ygor Gazola <ctl_ygazola@uolinc.com>
2023-05-22 21:18:30 -0300

    Primeira versao do bot simples

--- rasa/actions/default_fallback.py
+++ rasa/actions/default_fallback.py
@@ -1,16 +1,22 @@
 from typing import Any, Text, Dict, List
+
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import UserUtteranceReverted
 from rasa_sdk.executor import CollectingDispatcher
 
 class ActionDefaultFallback(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
     def name(self) -> Text:
-        return "action_default_fallback"
+        return "action_fallback"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # Mensagem de fallback padrão
-        message = "entrei aqui"
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Teste")
 
-        # Enviar a mensagem de fallback para o usuário
-        dispatcher.utter_message(text=message)
-
-        return []
+        return [UserUtteranceReverted()]
